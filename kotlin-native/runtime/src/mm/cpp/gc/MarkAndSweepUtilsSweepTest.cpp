961d788124d6620802ed7e87ee7d88d57a7ca13e
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2021-03-24 16:18:49 +0300

    Fix tests on Linux

--- kotlin-native/runtime/src/mm/cpp/gc/MarkAndSweepUtilsSweepTest.cpp
+++ kotlin-native/runtime/src/mm/cpp/gc/MarkAndSweepUtilsSweepTest.cpp
@@ -173,6 +173,16 @@ public:
                 RunFinalizers(node->IsArray() ? node->GetArrayHeader()->obj() : node->GetObjHeader());
             }
         }
+        testing::Mock::VerifyAndClear(&finalizerHook());
+        // TODO: Figure out a better way to clear up the stuff.
+        EXPECT_CALL(finalizerHook(), Call(testing::_)).Times(testing::AnyNumber());
+        for (auto node : objectFactory_.Iter()) {
+            auto* obj = node->IsArray() ? node->GetArrayHeader()->obj() : node->GetObjHeader();
+            if (auto* extraObject = mm::ExtraObjectData::GetOrNull(obj)) {
+                extraObject->ClearWeakReferenceCounter();
+            }
+            RunFinalizers(obj);
+        }
     }
 
     KStdVector<ObjHeader*> Sweep() {
