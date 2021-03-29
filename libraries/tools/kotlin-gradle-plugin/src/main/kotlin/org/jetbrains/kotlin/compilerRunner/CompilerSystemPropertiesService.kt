3fc22cdd1f21810b23fcaf7bd875e1953f4632e3
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2021-03-29 19:23:39 +0300

    [Gradle] Move Kotlin daemon system properties read to build service
    Instead of having a copy of those properties in each task move them into single build service

---
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/compilerRunner/CompilerSystemPropertiesService.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.compilerRunner
+
+import org.gradle.api.Project
+import org.gradle.api.invocation.Gradle
+import org.gradle.api.provider.MapProperty
+import org.gradle.api.provider.Provider
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+import org.jetbrains.kotlin.cli.common.CompilerSystemProperties
+import org.jetbrains.kotlin.gradle.utils.isGradleVersionAtLeast
+
+internal abstract class CompilerSystemPropertiesService : BuildService<CompilerSystemPropertiesService.Parameters>, AutoCloseable {
+    internal interface Parameters : BuildServiceParameters {
+        val properties: MapProperty<String, Provider<String>>
+    }
+
+    private val properties by lazy { parameters.properties.get().mapValues { it.value.orNull }.toMutableMap() }
+
+    fun startIntercept() {
+        if (!parameters.properties.isPresent) return
+
+        CompilerSystemProperties.systemPropertyGetter = {
+            if (it in properties) properties[it] else System.getProperty(it)
+        }
+        CompilerSystemProperties.systemPropertySetter = setter@{ key, value ->
+            val oldValue = properties[key]
+            if (oldValue == value) return@setter oldValue
+            properties[key] = value
+            System.setProperty(key, value)
+            oldValue
+        }
+        CompilerSystemProperties.systemPropertyCleaner = {
+            val oldValue = properties[it]
+            properties.remove(it)
+            System.clearProperty(it)
+            oldValue
+        }
+    }
+
+    override fun close() {
+        CompilerSystemProperties.systemPropertyGetter = null
+        CompilerSystemProperties.systemPropertySetter = null
+        CompilerSystemProperties.systemPropertyCleaner = null
+    }
+
+    companion object {
+        private val Project.isBuildSrc get() = name == ":buildSrc"
+
+        fun registerIfAbsent(gradle: Gradle): Provider<CompilerSystemPropertiesService> =
+            gradle.sharedServices.registerIfAbsent(
+                "${CompilerSystemPropertiesService::class.java.canonicalName}_${CompilerSystemPropertiesService::class.java.classLoader.hashCode()}",
+                CompilerSystemPropertiesService::class.java
+            ) { service ->
+                val rootProject = gradle.rootProject
+                // forUseAtConfigurationTime is available since Gradle 6.5
+                if (rootProject.isBuildSrc && isGradleVersionAtLeast(6, 5)) {
+                    service.parameters.properties.set(
+                        CompilerSystemProperties.values()
+                            .associate {
+                                it.property to rootProject.providers.systemProperty(it.property).forUseAtConfigurationTime()
+                            }.toMap()
+                    )
+                }
+            }
+    }
+}
